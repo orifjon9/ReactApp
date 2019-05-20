cb10b6a4bd825a04c0c4c5004bee899c80b1d17e
ORIFJON NARKULOV <orifjon9@gmail.com>
2019-05-19 19:37:22 -0600

    Usinf Radium for Media queries

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Person from './Person/Person';
-import Radium from 'radium';
+import Radium, { StyleRoot } from 'radium';
 import './App.css';
 
 class App extends Component {
@@ -84,12 +84,14 @@ class App extends Component {
 
 
         return (
-            <div className="App">
-                <h1> Hi, I 'm a React app!</h1>
-                <p className={classes.join(' ')}>This is really working</p>
-                <button style={style} onClick={this.togglePersonsHandler}>Toggle Persons</button>
-                {persons}
-            </div>
+            <StyleRoot>
+                <div className="App">
+                    <h1> Hi, I 'm a React app!</h1>
+                    <p className={classes.join(' ')}>This is really working</p>
+                    <button style={style} onClick={this.togglePersonsHandler}>Toggle Persons</button>
+                    {persons}
+                </div>
+            </StyleRoot>
         );
     }
 }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import Radium from 'radium';
 import './Person.css';
 
+
 const person = (props) => {
+    const style = {
+        '@media(min-width:500px)': {
+            width: '450px',
+            border: '1px solid red'
+        }
+    };
+
     return (
-        <div className="Person">
+        <div className="Person" style={style}>
             <p onClick={props.click}>My name is {props.name} and I am {props.age} year old.</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.changed} value={props.name} />
@@ -11,4 +20,4 @@ const person = (props) => {
     )
 };
 
-export default person;
+export default Radium(person);
