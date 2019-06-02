aea9f7712dd70122ce4874fcb213ab069234a842
ORIFJON NARKULOV <orifjon9@gmail.com>
2019-06-01 22:53:37 -0600

    Add Empty Wrap

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,17 +1,18 @@
 import React from 'react';
 // @ts-ignore
 import classes from './Person.css';
+import Aux from '../../../hoc/Auxiliary';
 
 class Person extends React.Component {
 
     render() {
         console.log('[Person.js] render');
         return (
-            <div className={classes.Person}>
+            <Aux className={classes.Person}>
                 <p onClick={this.props.click}>My name is {this.props.name} and I am {this.props.age} year old.</p>
                 <p>{this.props.children}</p>
                 <input type="text" onChange={this.props.changed} value={this.props.name} />
-            </div>
+            </Aux>
         )
     }
 }
